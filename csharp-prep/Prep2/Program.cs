d3f2eeb7d658b168f8ac3bbc653eb247511c7efe
Aubrey-M-115 <slitherraven19@gmail.com>
2026-01-17 22:45:13 -0700

    Prep 2 is done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,78 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        if (number >= 93)
+        {
+            Console.WriteLine("Congrats! You got an A!");
+        }
+
+        else if (number >= 90)
+        {
+            Console.WriteLine("Congrats! You got a A-!");
+        }
+
+        else if (number >= 87)
+        {
+            Console.WriteLine("Congrats! You got a B+!");
+        }
+
+        else if (number >= 83)
+        {
+            Console.WriteLine("Congrats! You got a B!");
+        }
+
+        else if (number >= 80)
+        {
+            Console.WriteLine("Congrats! You got a B-!");
+        }
+
+        else if (number >= 77)
+        {
+            Console.WriteLine("Congrats! You got a C+!");
+        }
+
+        else if (number >= 73)
+        {
+            Console.WriteLine("Congrats! You got a C!");
+        }
+
+        else if (number >= 70)
+        {
+            Console.WriteLine("Congrats! You got a C-!");
+        }
+
+        else if (number >= 67)
+        {
+            Console.WriteLine("Congrats! You got a D+!");
+        }
+
+        else if (number >= 63)
+        {
+            Console.WriteLine("Congrats! You got a D!");
+        }
+
+        else if (number >= 60)
+        {
+            Console.WriteLine("Congrats! You got a D-!");
+        }
+
+        else
+        {
+            Console.WriteLine("You got lower than a 60, so you got an F.");
+        }
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You have passed the class!");
+        }
+
+        else
+        {
+            Console.WriteLine("You have failed this class. Good luck next time.");
+        }
     }
 }
